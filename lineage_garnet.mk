b576fe940aebcd38bc23e198349941c9d7ce0c5c
zerotwo-alt <amruthv780@gmail.com>
2026-04-16 16:33:55 +0000

    garnet: enable common build flags
    Signed-off-by: zerotwo-alt <amruthv780@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_HAS_UDFPS := true
+TARGET_DISABLE_EPPE := true
+
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
