fbbd3cbb7c6d423ca29ed14fed1bfff2349a4a44
ZetyaWyn <amruthv780@gmail.com>
2026-08-01 13:25:18 +0530

    garnet: Switch to 8GB Dalvik heap config

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
